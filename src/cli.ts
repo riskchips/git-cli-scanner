f7469e6905c0e0112bcea3cc8c62828d277592f8
riskchips69 <arnabdas91das@gmail.com>
2026-09-06 21:44:49 +0530

    chore: bump version to 1.1.1 for publish

--- src/cli.ts
+++ src/cli.ts
@@ -13,7 +13,7 @@ const program = new Command();
 program
   .name('git-cli-scanner')
   .description('Interactive Git hooks vulnerability scanner')
-  .version('1.0.0');
+  .version('1.1.1');
 
 program
   .command('init')
